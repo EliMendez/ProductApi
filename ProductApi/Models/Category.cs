1235051851a8da43c201d8ebcac57238eae90585
Elizabeth Méndez <elixa_mendex@hotmail.com>
2024-12-15 22:14:21 -0600

    Add category model

---
+++ ProductApi/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductApi.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre de la categoría es obligatorio.")]
+        [StringLength(50, ErrorMessage = "El nombre de la categoría excede los 50 caracteres permitidos.")]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        [Required]
+        public DateTime UpdatedDate { get; set; }
+    }
+}
